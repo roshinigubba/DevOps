b4f0cc44c7942dc37525028f0d895dbf49aa076a
roshinigubba <153551746+roshinigubba@users.noreply.github.com>
2024-01-09 17:06:14 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA4OKKXQSRYIN2I5X5"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
