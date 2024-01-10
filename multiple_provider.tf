b814ddef854231b44fcdbd3349db9dc5b103a5c4
roshinigubba <153551746+roshinigubba@users.noreply.github.com>
2024-01-10 13:07:04 +0530

    Update multiple_provider.tf

--- multiple_provider.tf
+++ multiple_provider.tf
@@ -13,18 +13,18 @@ terraform {
 
 provider "aws" {
     region     = "us-east-1"
-    access_key = "AKIA4OKKXQSRYIN2I5X5"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
 }
 
 provider "azurerm" {
   features {}
-  subscription_id=" cd2aaeb7-d22a-4e4e-8b36-9485e29c3930"
-  tenant_id="f2f8c9a0-5656-429f-9bf1-bd181efad173"
+  subscription_id=""
+  tenant_id=""
 }
 
 resource "aws_instance" "example_aws_instance" {
-  ami           = "ami-0c7217cdde317cfec"
+  ami           = "ami-"
   instance_type = "t2.micro"
 
   tags = {
